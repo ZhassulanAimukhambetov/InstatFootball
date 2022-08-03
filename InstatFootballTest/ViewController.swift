ffc512a378b7888948c196be86ca87d823ac7548
Zhassulan Aimukhambetov <kolesa@Zhassulans-MacBook-Pro.local>
2022-08-04 00:55:13 +0600

    create project

---
+++ InstatFootballTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstatFootballTest
+//
+//  Created by Zhassulan Aimukhambetov on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
